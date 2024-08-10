7537264adce56be652ccb8c56257c8f11cdc9812
Zack Morgenthaler <zackmorgenthaler@gmail.com>
2024-08-10 10:40:56 -0500

    added more pages, added custom font

---
+++ about.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+  <?php include 'head.php'; ?>
+</head>
+
+<body>
+  <?php include 'nav.php'; ?>
+  <main>
+    <section>
+      <h1>About</h1>
+    </section>
+  </main>
+  <?php include 'footer.php'; ?>
+  <script src="js/app.js"></script>
+</body>
+
+</html>
--- nav.php
+++ nav.php
@@ -6,9 +6,18 @@
     </div>
     <ul id="nav_list">
         <?php $nav_items = [
-            ['url' => 'index.php', 'label' => 'Home'],
-            ['url' => 'about.php', 'label' => 'About'],
-            ['url' => 'contact.php', 'label' => 'Contact']
+            [
+                'url' => 'index.php', 
+                'label' => 'Home'
+            ],
+            [
+                'url' => 'about.php', 
+                'label' => 'About'
+            ],
+            [
+                'url' => 'contact.php', 
+                'label' => 'Contact'
+            ]
         ]; ?>
         <?php for ($i=0; $i < count($nav_items); $i++) { ?>
             <li class="nav_list_item">
