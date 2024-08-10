516134cbe6b1d05c89620d74635b8b4d7db65fa1
Zack Morgenthaler <zackmorgenthaler@gmail.com>
2024-08-10 09:25:23 -0500

    initial commit

---
+++ footer.php
@@ -0,0 +1,3 @@
+<footer>
+    <p>&copy; 2021, All rights reserved by www.abc.com</p>
+</footer>
---
+++ nav.php
@@ -0,0 +1,21 @@
+<nav id="main_nav">
+    <div id="nav_bar">
+        <a href="index.php">
+            <img id="logo" src="icon.png" alt="logo">
+        </a>
+    </div>
+    <ul id="nav_list">
+        <?php $nav_items = [
+            ['url' => 'index.php', 'label' => 'Home'],
+            ['url' => 'about.php', 'label' => 'About'],
+            ['url' => 'contact.php', 'label' => 'Contact']
+        ]; ?>
+        <?php for ($i=0; $i < count($nav_items); $i++) { ?>
+            <li>
+                <a href="<?php echo $nav_items[$i]['url']; ?>">
+                    <?php echo $nav_items[$i]['label']; ?>
+                </a>
+            </li>
+        <?php } ?>
+    </ul>
+</nav>
